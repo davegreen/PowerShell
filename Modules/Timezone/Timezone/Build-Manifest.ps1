944edf6b60a5dd6ee1f1b83f6f0ae18deaefc966
Dave Green <david.green@tookitaway.co.uk>
2016-04-17 20:32:32 +0100

    Rework manifest

--- Modules/Timezone/Timezone/Build-Manifest.ps1
+++ Modules/Timezone/Timezone/Build-Manifest.ps1
@@ -6,4 +6,31 @@
 	
 #>
 
-New-ModuleManifest -Path "$PSScriptRoot\Timezone.psd1" -Description 'A PowerShell script module designed to get and set the timezone, wrapping the tzutil command.' -RootModule 'Timezone.psm1' -Author 'David Green' -CompanyName 'http://tookitaway.co.uk/, https://github.com/davegreen/PowerShell/' -Copyright '(c) 2016 David Green. All rights reserved.' -PowerShellVersion '5.0' -ModuleVersion '1.0.2' -FileList @('Timezone.psd1', 'Timezone.psm1', 'Timezone.Tests.ps1') -FunctionsToExport @('Get-Timezone', 'Get-TimezoneFromOffset', 'Set-Timezone') -CmdletsToExport @('Get-Timezone', 'Get-TimezoneFromOffset', 'Set-Timezone') -VariablesToExport $null -AliasesToExport @('Get-Timezone', 'Get-TimezoneFromOffset', 'Set-Timezone')
+$PSData = @{
+    Tags = @('Timezone', 'tzutil')
+    LicenseUri = 'https://github.com/davegreen/PowerShell/blob/master/LICENSE'
+    ProjectUri = 'http://tookitaway.co.uk'
+    # IconUri = ''
+    # ReleaseNotes = ''
+}
+
+$CmdLets = @('Get-Timezone', 'Get-TimezoneFromOffset', 'Set-Timezone')
+
+$ModuleDescription = @{
+    Path = "$PSScriptRoot\Timezone.psd1"
+    Description = 'A PowerShell script module designed to get and set the timezone, wrapping the tzutil command.'
+    RootModule = 'Timezone.psm1'
+    Author = 'David Green'
+    CompanyName = 'http://tookitaway.co.uk/, https://github.com/davegreen/PowerShell/'
+    Copyright = '(c) 2016 David Green. All rights reserved.'
+    PowerShellVersion = '5.0'
+    ModuleVersion = '1.0.3' 
+    FileList = @('Timezone.psd1', 'Timezone.psm1', 'Timezone.Tests.ps1') 
+    FunctionsToExport = $CmdLets
+    CmdletsToExport = $CmdLets
+    VariablesToExport = $null
+    AliasesToExport = $CmdLets
+    PrivateData = $PSData
+}
+
+New-ModuleManifest @ModuleDescription
