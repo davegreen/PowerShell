14b3998a5f978ee2be89519a957ee5e08c5e8219
Dave Green <david.green@tookitaway.co.uk>
2016-04-17 23:44:45 +0100

    fix manifest

--- Modules/Timezone/Timezone/Build-Manifest.ps1
+++ Modules/Timezone/Timezone/Build-Manifest.ps1
@@ -6,14 +6,6 @@
 	
 #>
 
-$PSData = @{
-    Tags = @('Timezone', 'tzutil')
-    LicenseUri = 'https://github.com/davegreen/PowerShell/blob/master/LICENSE'
-    ProjectUri = 'http://tookitaway.co.uk'
-    # IconUri = ''
-    # ReleaseNotes = ''
-}
-
 $CmdLets = @('Get-Timezone', 'Get-TimezoneFromOffset', 'Set-Timezone')
 
 $ModuleDescription = @{
@@ -24,13 +16,17 @@ $ModuleDescription = @{
     CompanyName = 'http://tookitaway.co.uk/, https://github.com/davegreen/PowerShell/'
     Copyright = '(c) 2016 David Green. All rights reserved.'
     PowerShellVersion = '5.0'
-    ModuleVersion = '1.1' 
-    FileList = @('Timezone.psd1', 'Timezone.psm1', 'Timezone.Tests.ps1') 
+    ModuleVersion = '1.1,1'
+    FileList = @('Timezone.psd1', 'Timezone.psm1', 'Timezone.Tests.ps1', 'Timezone.Help.Tests.ps1') 
     FunctionsToExport = $CmdLets
     CmdletsToExport = $CmdLets
     VariablesToExport = $null
     AliasesToExport = $CmdLets
-    PrivateData = $PSData
+    Tags = @('Timezone', 'tzutil')
+    LicenseUri = 'https://github.com/davegreen/PowerShell/blob/master/LICENSE'
+    ProjectUri = 'http://tookitaway.co.uk'
+    # IconUri = ''
+    # ReleaseNotes = ''
 }
 
 New-ModuleManifest @ModuleDescription
