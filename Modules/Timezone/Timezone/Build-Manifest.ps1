00433945bca10cbeb14c0d4009e9c56624b90ba2
Dave Green <david.green@tookitaway.co.uk>
2016-06-11 14:48:28 +0100

    Update build manifest to generalise

--- Modules/Timezone/Timezone/Build-Manifest.ps1
+++ Modules/Timezone/Timezone/Build-Manifest.ps1
@@ -6,25 +6,44 @@
 	
 #>
 
-$Module   = 'Timezone'
-$CmdLets  = @('Get-Timezone', 'Set-Timezone')
-$FileList = (Get-ChildItem -Path $PSScriptRoot -Exclude 'Build-Manifest.ps1').Name + "$Module.psd1"
+$ModuleBase = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+# Handles modules in version directories
+$leaf          = Split-Path $ModuleBase -Leaf
+$parent        = Split-Path $ModuleBase -Parent
+$parsedVersion = $null
+
+if ([System.Version]::TryParse($leaf, [ref]$parsedVersion)) {
+	$ModuleName = Split-Path $parent -Leaf
+}
+
+else {
+	$ModuleName = $leaf
+}
+
+# Removes all versions of the module from the session before importing
+Get-Module $ModuleName | Remove-Module
+
+# Because ModuleBase includes version number, this imports the required version
+# of the module
+$Module   = Import-Module $ModuleBase\$ModuleName.psm1 -PassThru -ErrorAction Stop
+$commands = Get-Command -Module $Module
 
 $ModuleDescription = @{
-    Path              = "$PSScriptRoot\$Module.psd1"
+    Path              = "$PSScriptRoot\$ModuleName.psd1"
     Description       = 'A PowerShell script module designed to get and set the timezone, wrapping the tzutil command.'
-    RootModule        = "$Module.psm1"
+    RootModule        = "$ModuleName.psm1"
     Author            = 'David Green'
     CompanyName       = 'http://tookitaway.co.uk/, https://github.com/davegreen/PowerShell/'
     Copyright         = '(c) 2016 David Green. All rights reserved.'
     PowerShellVersion = '5.0'
     ModuleVersion     = '1.2.2'
-    FileList          = $FileList
-    FunctionsToExport = $CmdLets
-    CmdletsToExport   = $CmdLets
+    FileList          = ((Get-ChildItem -Recurse -File -Path $PSScriptRoot).Name | Where-Object { $_ -ne 'Build-Manifest.ps1' })
+    FunctionsToExport = $commands.Name
+    CmdletsToExport   = $commands.Name
     VariablesToExport = $null
-    AliasesToExport   = $CmdLets
-    Tags              = @($Module, 'tzutil')
+    AliasesToExport   = $commands.Name
+    Tags              = @($ModuleName, 'tzutil')
     LicenseUri        = 'https://github.com/davegreen/PowerShell/blob/master/LICENSE'
     ProjectUri        = 'http://tookitaway.co.uk'
     # IconUri         = ''
